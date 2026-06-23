6c804c9857d057e1949713d06e3a8c7c5e459467
Soham553 <sakharesoham10@gmail.com>
2026-06-23 16:19:06 +0530

    Format stack using array

--- Stack/Stack_Using_Array.cpp
+++ Stack/Stack_Using_Array.cpp
@@ -1,41 +1,50 @@
 #include <iostream>
 using namespace std;
 
+class stack
+{
+public:
+	int *arr;
+	int size;
+	int top;
 
-class stack{
-	public:
-		int *arr;
-		int size;
-		int top;
+	stack(int size)
+	{
+		this->size = size;
+		arr = new int[size];
+		top = -1;
+	}
 
-		stack(int size){
-			this -> size = size;
-			arr = new int[size];
-			top = -1;
+	void push(int element)
+	{
+		if (size - top > 1)
+		{
+			top++;
+			arr[top] = element;
 		}
-
-		void push(int element){
-			if(size - top > 1){
-				top++;
-				arr[top] = element;
-			}else{
-				cout<<"Stack overflow"<<endl;				}
-
+		else
+		{
+			cout << "Stack overflow" << endl;
 		}
+	}
 
-		void pop(){
-			if(top >= 0){
-				top--;
-				cout<<arr[top]<<"   is removed"<<endl;
-			}else{
-				cout<<"stack overflow"<<endl;
-			}
+	void pop()
+	{
+		if (top >= 0)
+		{
+			top--;
+			cout << arr[top] << "   is removed" << endl;
 		}
-
+		else
+		{
+			cout << "stack overflow" << endl;
+		}
+	}
 };
 
-int main(){
-   	stack s(5);
+int main()
+{
+	stack s(5);
 	s.push(4);
 	s.push(4);
 	s.push(5);
